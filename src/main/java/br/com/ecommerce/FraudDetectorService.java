48b56dd74e4f2029a86c2e4c0c4e8f976c7c8042
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2021-06-11 09:20:44 -0300

    até parte 5- aula 4

--- src/main/java/br/com/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/ecommerce/FraudDetectorService.java
@@ -1,46 +1,33 @@
 package br.com.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class FraudDetectorService {
-    public static void main(String[] args) throws InterruptedException {
-        var consumer = new KafkaConsumer<String,String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER")); //RARO escutar mais de um tópico
 
-        while(true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            if (!records.isEmpty()) {
-                System.out.println("Encontrei " + records.count() + " registros");
-                //return;
-                for (var record : records) {
-                    System.out.println("----------------------------------------------");
-                    System.out.println("Processando new order, checking for fraud...");
-                    System.out.println("KEY: " + record.key());
-                    System.out.println("VALUE: " + record.value());
-                    System.out.println("PARTITION: " + record.partition());
-                    System.out.println("OFFSET: " + record.offset());
-                    Thread.sleep(2000);
-                    System.out.println("----------------------------------------------");
-                    System.out.println("Processou!");
-                }
-            }
+    public static void main(String[] args) {
+        var fraudService = new FraudDetectorService();
+        try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse,
+                Order.class)) {
+            service.run();
         }
+    }
 
+    private void parse(ConsumerRecord<String, Order> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Order processed");
     }
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName()); // #nome do grupo
-        return properties;
-    }
+
 }
--- src/main/java/br/com/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/ecommerce/NewOrderMain.java
@@ -1,38 +1,28 @@
 package br.com.ecommerce;
 
-import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Map;
-import java.util.Properties;
+import java.math.BigDecimal;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        //<chave, mensagem>
-        var producer = new KafkaProducer<String, String>(properties());
-        var value = "123,456,52.89";
-        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER",value,value);
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
+
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    var email = "Thank you for your order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
-            System.out.println("Sucesso no envio! " + data.topic() + " - partition:" + data.partition() + " | offset: " + data.offset() + " | timestamp: " + data.timestamp());
-
-        };
-        producer.send(record, callback).get();//assíncrono
-
-        var email = "Welcome! We're processing your order! ";
-        var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL",email,email);
-        producer.send(emailRecord,callback).get();
+        }
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
-    }
 }
